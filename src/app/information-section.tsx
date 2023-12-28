34e45b27cd7bd8e43ada8c3b0349bf6db361b966
Usman Bhat <74877966+Usman-bhat@users.noreply.github.com>
2023-12-28 02:09:51 +0200

    Update information-section.tsx

--- src/app/information-section.tsx
+++ src/app/information-section.tsx
@@ -20,7 +20,7 @@ export function InformationSection() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://127.0.0.1:3000/api");
+        const response = await fetch("https://alamaiqbal.vercel.app/api");
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
