0ae1a38fe4b7a58058fb148034961ff946dc9b7d
Usman Bhat <74877966+Usman-bhat@users.noreply.github.com>
2023-12-28 09:50:19 +0200

    Update route.tsx

--- src/app/api/[bookId]/[poemId]/route.tsx
+++ src/app/api/[bookId]/[poemId]/route.tsx
@@ -1,7 +1,24 @@
- import sqlite3 from "sqlite3";                                                                            import { open } from "sqlite";                  
-   let db = null;                                                                                            interface PoemID{                                                                                           poemId:string
+import sqlite3 from "sqlite";
+import { open } from "sqlite";
+
+let db = null;
+
+interface PoemID {
+  poemId: string;
 }
-export async function GET(request:NextRequest,{params}:{params:PoemID}) {                                         if (!db) {                                                                                                  db = await open({                                                                                           filename: "data.db",                                                                                      driver: sqlite3.Database,                                                                               });  
-       }                                                                                                       
-/*const {bookId} = params.params.bookId;
-//console.log(params.bookId);        */                                                                                                                                                                              const todos = await db.all("SELECT _id,title,data FROM POEMS where _id = ? ",params.poemId);                                                                                                                          return new Response(JSON.stringify(todos), {                                                                headers: { "content-type": "application/json" },                                                          status: 200,                                                                                            });                                                                                                     }
+
+export async function GET(request: NextRequest, { params }: { params: PoemID }) {
+  if (!db) {
+    db = await open({
+      filename: "data.db",
+      driver: sqlite3.Database,
+    });
+  }
+
+  const todos = await db.all("SELECT _id, title, data FROM POEMS where _id = ?", params.poemId);
+
+  return new Response(JSON.stringify(todos), {
+    headers: { "content-type": "application/json" },
+    status: 200,
+  });
+}
