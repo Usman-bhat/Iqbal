7099a1b5ce95ba46d83c9be0df5132e19a16d1d4
Usman Bhat <74877966+Usman-bhat@users.noreply.github.com>
2023-12-30 23:34:30 +0200

    Update Getlist.tsx

--- src/app/[bookId]/Getlist.tsx
+++ src/app/[bookId]/Getlist.tsx
@@ -1,58 +1,82 @@
-"use client";                                                                                             import React, { useState, useEffect } from 'react';                                                       import { Typography,Alert } from "@material-tailwind/react";
+"use client";
+import React, { useState, useEffect } from 'react';
+import { Typography, Alert } from "@material-tailwind/react";
 import { AcademicCapIcon } from "@heroicons/react/24/solid";
-import {ListCard} from "@/components/list-card"; // Adjust the import path as needed
-import { useTheme } from '@/app/ThemeProvider';  // Adjust the path accordingly
-
+import { ListCard } from "@/components/list-card"; // Adjust the import path as needed
+import { useTheme } from '@/app/ThemeProvider';  // Adjust the path accordingly                           
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import { db } from "@/app/firebase";
+import { collection, getDocs, query, limit, where, orderBy } from "firebase/firestore";
 
+interface Item {
+  title: string;
+  _id: string;
+  // Add other fields as needed
+}
 
+async function fetchFirebase(bookid) {
+  console.log(bookid);
+  const qrysnap = await getDocs(query(collection(db, "poems"), orderBy("_id"), where("book_id", "==", parseInt(bookid))));
 
-interface InfoCard {bookId: string; }
+  const data = [];
+  qrysnap.forEach((doc) => {
+    data.push({ id: doc.id, ...doc.data() });
+  });
+  return data;
+}
 
-
-
-export function Getlist({bookId}:InfoCard) {
-  const { darkTheme } = useTheme();                                                                                                                                                                                   const [items, setItems] = useState([]);
+export function Getlist({ bookId }) {
+  const { darkTheme } = useTheme();
+  const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);                                                               
-  useEffect(() => {                                                                                           const fetchData = async () => {                                                                             try {
-        const response = await fetch(`https://alamaiqbal.vercel.app/api/${bookId}`);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchFirebase1 = async () => {
+      try {
+        const data = await fetchFirebase(bookId);
         setItems(data);
       } catch (error) {
-        setError(error);
+        setError();
       } finally {
         setLoading(false);
-      }                                                                                                       };
-                                                                                                              fetchData();
+      }
+    };
+
+    fetchFirebase1();
   }, []);
 
   if (loading) {
-	  return (<Skeleton height={30} count={5} />);
+    return (<Skeleton height={30} count={5} />);
   }
 
- if (error) {
-  return (<Alert className="m-4">Oops!!! somthing went wrong.Please refresh </Alert>)
-}
-const collection = items.map((item, idx) => (
-    <ListCard
-      key={idx}
-      bname={item.title}
-      id={item._id}
-      bookId={bookId}
-    />
-  ));
-                                                                                                            return (                                                                                                    <>
-      <section className={`pb-28 px-8 ${darkTheme ? 'bg-gray-800' : 'bg-gray-100'}`}>                                                                                                                                              <div className="m-4 text-center">
-              <Typography color={darkTheme ? 'gray' : 'blue-gray'} className={`mb-2 text-center mt-3text-3xl font-bold ${darkTheme ? 'text-gray-100' : 'blue-gray'}`} >
-Poems
-              </Typography>
-            </div>
-         <div className="flex flex-col flex-row h-full grid gap-4 sm:grid-col-2 md:grid-cols-2 lg:grid-cols-3">
+  if (error) {
+    return (<Alert className="m-4 p-2">Oops!!! something went wrong. Please refresh</Alert>);
+  }
+
+  const collection = items.length > 0 ? (
+    items.map((item, idx) => (
+      <ListCard
+        key={item._id}
+        bname={item.title}
+        id={item._id}
+        bookId={bookId}
+      />
+    ))
+  ) : (
+    <Alert className="m-4 p-2">No Data Found</Alert>
+  );
+
+  return (
+    <>
+      <section className={`pb-28 px-8 ${darkTheme ? 'bg-gray-800' : 'bg-gray-100'}`}>
+        <div className="m-4 text-center">
+          <Typography placeholder="poem" className={`mb-2 text-center mt-3 text-3xl font-bold ${darkTheme ? 'text-gray-100' : 'blue-gray'}`}>
+            Poems
+          </Typography>
+        </div>
+        <div className="flex flex-col flex-row h-full grid gap-4 sm:grid-col-2 md:grid-cols-2 lg:grid-cols-3">
           {collection}
         </div>
       </section>
