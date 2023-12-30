c3317edbd682b5dfe53220f69a1c3ace31aabd36
Usman Bhat <74877966+Usman-bhat@users.noreply.github.com>
2023-12-31 00:30:34 +0200

    Update Getpoem.tsx

--- src/app/[bookId]/[poemId]/Getpoem.tsx
+++ src/app/[bookId]/[poemId]/Getpoem.tsx
@@ -7,29 +7,32 @@ import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { useTheme } from "@/app/ThemeProvider"; // Adjust the path accordingly
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import { db } from "@/app/firebase";
+import { collection, getDocs, query,where } from "firebase/firestore";
 
-export function Getpoem({ bookId,poemId }) {
+
+async function fetchFirebase(poemid) {
+  const qrysnap = await getDocs(query(collection(db, "poems"), where("_id", "==", parseInt(poemid))));
+  if (qrysnap.empty) {
+    throw new Error("No document found with the specified ID");
+  }
+  const doc = qrysnap.docs[0].data();
+  return { id: qrysnap.docs[0].id, ...doc };
+}
+
+export function Getpoem({ bookId, poemId }) {
   const { darkTheme } = useTheme();
-   const next = parseInt(poemId)+1;
-   const prev = parseInt(poemId) > 1 ? parseInt(poemId) - 1 : 1;
-
-  const [items, setItems] = useState([]);
+  const next = parseInt(poemId) + 1;
+  const prev = parseInt(poemId) > 1 ? parseInt(poemId) - 1 : 1;
+  const [item, setItem] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-//console.log(bookId);
   useEffect(() => {
     const fetchData = async () => {
       try {
-	console.log(bookId);
-        const response = await fetch(`https://alamaiqbal.vercel.app/api/${bookId}/${poemId}`);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        setItems(data);
-      } catch (error) {
-        setError(error);
+        const data = await fetchFirebase(poemId);
+        setItem(data);
       } finally {
         setLoading(false);
       }
@@ -42,60 +45,34 @@ export function Getpoem({ bookId,poemId }) {
     return <Skeleton height={70} count={5} />;
   }
 
-  if (error) {
-    return (<Alert className="m-4">Oops!!! somthing went wrong.Please refresh </Alert>)
+  if (error) {                                                                                                return <Alert className="m-4">Oops!!! Something went wrong. Please refresh.</Alert>;
+  }
+ if (!item || Object.keys(item).length === 0) {
+    return <Alert className="m-4">No data found.</Alert>;
   }
 
-  const testimonialCards = items.map((item, idx) => (
-
-
-
-    <Card key={idx} color="transparent" shadow={false} className="p-2">
-      <CardBody>
-        <div className={`mb-2 text-center text-lg ${darkTheme ? 'text-gray-100 bg-gray-800' : 'text-gray-800 bg-gray-200'}`}>
-          {item.title}
-        </div>
-	<div class="border-t border-gray-300 mb-4"></div>	
-
-
-        <div class={`whitespace-pre direction-rtl text-center ${darkTheme ? 'text-white' : 'text-black'}`}>
-          {item.data}
-        </div>
-      </CardBody>
-    </Card>
-  ));
-
   return (
-    <section className={`py-10 px-4 lg:py-18 ${darkTheme ? 'bg-gray-800' : 'bg-gray-200'}`}>
-      <div className="flex flex-col flex-row h-full">
-        {testimonialCards}
+    <section className={`py-10 px-4 lg:py-18 ${darkTheme ? 'bg-gray-800' : 'bg-gray-200'}`}>                    <Card placeholder="" key={item.id} color="transparent" shadow={false} className="p-2">                      <CardBody placeholder="">                                                                                   <div className={`mb-2 text-center text-lg ${darkTheme ? 'text-gray-100 bg-gray-800' : 'text-gray-800 bg-gray-200'}`}>                                                                                                 {item.title}
+          </div>
+          <div className="border-t border-gray-300 mb-4"></div>
+          <div className={`whitespace-pre direction-rtl text-center ${darkTheme ? 'text-white' : 'text-black'}`}>
+            {item.data}
+          </div>
+        </CardBody>
+      </Card>
+
+      <div className={`flex justify-between bg-gray-100 px-4 ${darkTheme ? 'bg-gray-800' : 'bg-gray-200'}`}>
+        <div className={`flex items-center outline m-4 p-4 rounded-md ${darkTheme ? 'bg-gray-600' : 'bg-gray-100'}`}>
+          <a href={`/${bookId}/${prev}`} className="blue-gray hover:underline cursor-pointer">
+            <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" />
+          </a>
+        </div>
+        <div className={`flex items-center outline m-4 p-4 rounded-md bg-gray-100 ${darkTheme ? 'bg-gray-600' : 'bg-gray-100'}`}>
+          <a href={`/${bookId}/${next}`} className="blue-gray hover:underline cursor-pointer">
+            <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
+          </a>
+        </div>
       </div>
-
-
-  <div className={`flex justify-between bg-gray-100 px-4 ${darkTheme ? 'bg-gray-800' :'bg-gray-200'}`}>
-
-      <div className={`flex items-center outline m-4 p-4 rounded-md ${darkTheme ? 'bg-gray-600' :'bg-gray-100'}`}>
-        <a href={`/${bookId}/${prev}`} className=" blue-gray hover:underline cursor-pointer">
-	   <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" />
-        </a>
-      </div>
-
-      <div className={`flex items-center outline m-4 p-4  rounded-md bg-gray-100 ${darkTheme ? 'bg-gray-600' :'bg-gray-100'}`}>
-        <a href={`/${bookId}/${next}`} className="blue-gray hover:underline cursor-pointer">
-	<ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
-	</a>
-      </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
     </section>
   );
 }
