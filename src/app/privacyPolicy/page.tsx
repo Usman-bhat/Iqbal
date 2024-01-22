44d6d11ed02e43ecee6340fb9932ea3d21579cd3
Usman Bhat <74877966+Usman-bhat@users.noreply.github.com>
2024-01-22 09:51:41 +0200

    Create page.tsx

---
+++ src/app/privacyPolicy/page.tsx
@@ -0,0 +1,26 @@
+// components/PrivacyPolicy.js
+
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="max-w-xl mx-auto my-8 p-4 bg-white rounded shadow">
+      <h2 className="text-2xl font-bold mb-4">Privacy Policy:</h2>
+      <p className="mb-4">
+        Our Allama Iqbal app respects your privacy. We do not collect or share
+        user data with any third-party services. Any data you generate in the
+        app, such as preferences, themes, fonts, bookmarks, or notes, is stored
+        only on your device. Clearing the app's data or uninstalling it removes
+        this information.
+      </p>
+      <p className="mb-4">
+        If you identify a security vulnerability unintentionally caused by the
+        app or have privacy-related inquiries, kindly email us. We are committed
+        to addressing and resolving any concerns promptly.
+      </p>
+      <p className="font-semibold">Yours sincerely,QuranicHub Team</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
